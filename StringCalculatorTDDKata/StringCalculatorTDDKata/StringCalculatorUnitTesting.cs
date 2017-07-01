16199e38e10118cf8c9d7ecc7bb57ac2249671ed
Chris Hunter <chris-2k6@hotmail.co.uk>
2017-07-01 23:12:16 +0100

    GivenSingleCharString test failure
    Commit to show GivenSingleCharString test failure

---
+++ StringCalculatorTDDKata/StringCalculatorTDDKata/StringCalculatorUnitTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorTDDKata
+{
+    [TestClass]
+    public class StringCalculatorUnitTesting
+    {
+        [TestMethod]
+        public void GivenSingleCharString()
+        {
+            Calculator calculator = new Calculator();
+            var calculationResult = calculator.Calculate("1");
+            Assert.AreEqual(calculationResult, 1);
+        }
+    }
+
+    public class Calculator
+    {
+        public double Calculate(string value)
+        {
+            return 0;
+        }
+    }
+}
